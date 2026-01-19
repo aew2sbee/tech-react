9eadffaf70b77d564377a12483bd20aeecc9f52a
aew2sbee <aew2sbee@gmail.com>
2026-01-19 22:22:59 +0900

    feat: HomeコンポーネントとMyButtonコンポーネントを追加

---
+++ chapter01/app/nest/page.tsx
@@ -0,0 +1,10 @@
+import { MyButton } from "@/components/Button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
