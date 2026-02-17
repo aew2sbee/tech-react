6eba08044447e431e3574ac5625a0fcfe358ccd4
Furuta Naoki <naoki.furuta@japan-systems.co.jp>
2026-02-17 14:05:46 +0900

    feat: 404ページのスタイルを改善し、CSSモジュールを追加

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/poc-react", // The name of your repository
+  assetPrefix: "/poc-react", // The name of your repository
   output: 'export', // Enables static exports for GitHub Pages
   reactCompiler: true,
 };
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,16 @@
 import Link from 'next/link';
+import styles from './page.module.css';
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-      <Link href="/">Go back to Home</Link>
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <div className={styles.intro}>
+          <h1>404 - Page Not Found</h1>
+          <p>The page you are looking for does not exist.</p>
+          <Link href="/">Go back to Home</Link>
+        </div>
+      </main>
     </div>
   );
 }
